3e1fa0cae37a4e2d3ac26f975b4c2a5433e84d42
gattosazio <your-new-email@example.com>
2026-05-26 14:02:21 +0800

    node 2 config

--- src/domain/config.h
+++ src/domain/config.h
@@ -3,7 +3,7 @@
 
 namespace Config {
 constexpr char FIREBASE_URL[] =
-    "https://lte-test2-default-rtdb.asia-southeast1.firebasedatabase.app/Node1.json";
+    "https://lte-test2-default-rtdb.asia-southeast1.firebasedatabase.app/Node2.json";
 
 constexpr unsigned long SENSOR_READ_INTERVAL_MS = 3000UL;
 constexpr unsigned long FIREBASE_WET_INTERVAL_MS = 300000UL;
@@ -22,7 +22,7 @@ constexpr unsigned int FIREBASE_PERSISTENT_FAIL_REBOOT_COUNT = 5;
 
 constexpr float RAIN_MM_PER_TIP = 0.70f;
 constexpr unsigned long RAIN_DEBOUNCE_MS = 500UL;
-constexpr unsigned long RAIN_WINDOW_MS = 600000UL;
+constexpr unsigned long RAIN_WINDOW_MS = 1800000UL;
 constexpr int RAIN_STARTUP_IGNORED_TIPS = 2;
 constexpr int RAIN_MAX_TIPS = 300;
 
@@ -30,10 +30,10 @@ constexpr unsigned int TIP_PIN = 33;
 
 constexpr unsigned int ULTRASONIC_TRIG_PIN = 18;
 constexpr unsigned int ULTRASONIC_ECHO_PIN = 32;
-constexpr int ULTRASONIC_SAMPLES = 9;
-constexpr unsigned long ULTRASONIC_SAMPLE_DELAY_MS = 60UL;
-constexpr float ULTRASONIC_EMPTY_DISTANCE_CM = 225.0f;
-constexpr float ULTRASONIC_MAX_WATER_LEVEL_CM = 55.0f;
+constexpr int ULTRASONIC_SAMPLES = 15;
+constexpr unsigned long ULTRASONIC_SAMPLE_DELAY_MS = 70UL;
+constexpr float ULTRASONIC_EMPTY_DISTANCE_CM = 157.0f;
+constexpr float ULTRASONIC_MAX_WATER_LEVEL_CM = 75.0f;
 constexpr float ULTRASONIC_MIN_VALID_CM = 5.0f;
 constexpr float ULTRASONIC_MAX_VALID_CM = 450.0f;
 constexpr float ULTRASONIC_SMOOTHING_ALPHA = 0.25f;
